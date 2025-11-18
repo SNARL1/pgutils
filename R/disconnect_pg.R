f5fc2108da94fa2d4b938a98160736ab05071a99
Roland Knapp <roland.knapp@ucsb.edu>
2025-11-18 12:47:55 -0800

    Simplify disconnect

--- R/disconnect_pg.R
+++ R/disconnect_pg.R
@@ -1,3 +1,32 @@
+#' Disconnect a PostgreSQL connection
+#'
+#' Safely closes a connection created with `connection_open()` from the connections package
+#' and removes the connection object from the environment.
+#'
+#' @param con A connection object returned by `connect_to_pg()`
+#' @export
+disconnect_pg <- function(con) {
+  if (!inherits(con, "connConnection")) {
+    warning("Provided object is not a connection from the connections package.")
+    return(invisible(NULL))
+  }
+  
+  connections::connection_close(con)
+  
+  # Remove the object from parent environment
+  con_name <- deparse(substitute(con))
+  if (exists(con_name, envir = parent.frame())) {
+    rm(list = con_name, envir = parent.frame())
+  }
+  
+  message("Connection closed and object removed: ", con_name)
+  invisible(NULL)
+}
+
+
+
+
+
 #' Disconnect from PostgreSQL
 #'
 #' Works with connections created using the RStudio connections pane
